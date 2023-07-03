0a5a3ef3d613ed0d04ac9b3a4ab68ed469571524
Danielle Moore <danielle.moore0214@gmail.com>
2023-07-02 22:46:55 -0400

    Attempting to run the whole thing 5 times Did not work only run once.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const choices =[ "rock", "paper", "scissors"];
+let playerScore = 0;
+    let computerScore = 0;
 function getComputerChoice(choices){
     
     const index = Math.floor(Math.random() * choices.length);
@@ -11,16 +13,31 @@ function playGame(computerSelection, playerSelection){
     } else if( playerSelection == 'rock' && computerSelection=='scissors' ||
     playerSelection=='scissors' && computerSelection =='paper'||
     playerSelection=='paper' && computerSelection=='rock'){
+        playerScore++;
         return "You Win";
     } else{
+        computerScore++;
         return "You LOSE!";
     } 
 }
 
-const computerSelection = getComputerChoice(choices);
-const playerSelection = prompt("Rock, paper, or scissors");
-let player = playerSelection.toLowerCase();
+function gameTo5(){
+    
+    for(i=0; i< 5; i++){
+        let playerSelection = prompt("Rock, paper or scissors");
+        console.log("You chose: " + playerSelection.toLowerCase());
+        let computerSelection = getComputerChoice(choices);
+        console.log("Computer Chose: " + computerSelection)
+        playGame(computerSelection, playerSelection)
+       if (playerScore > computerScore){
+        return "You Win";
+       } else if (playerScore < computerScore){
+        return "You Lose";
+       }else {
+        return "You both win"
+       }
+    }
+}
 
-console.log("You chose: " +player)
-console.log("Computer Chose: " + computerSelection)
-console.log(playGame(computerSelection,player))
+console.log(gameTo5());
+console.log("Your score was " + playerScore + "Computer score was "+ computerScore)
